54d4f4c49596fba85cada4f1c68048b2015a84df
わたり。 <yue.hinako.0175@gmail.com>
2019-08-02 15:15:08 +0900

    useraction, topicsindex-view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,16 +1,30 @@
 class TopicsController < ApplicationController
 
   def index
-    
+    @topics = Topic.limit(20).order('id ASC')
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to controller: :topics, action: :index
+    else
+      reder :new, notice: "スレッドを作成できませんでした"
+    end
   end
 
   def search
+    @topics = Topic.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :video_url, :user_id)
   end
 
 end
